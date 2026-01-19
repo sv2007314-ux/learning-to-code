58cbb9b0f80a707d1b6641bc02c34b3abbc18783
sv2007314-ux <sv2007314@gmail.com>
2026-01-20 00:12:10 +0530

    Create Task 1.py

---
+++ Task 1.py
@@ -0,0 +1,9 @@
+num1=float(input("enter the first number"))
+num2=float(input("enter the second number"))
+print("addition",num1+num2)
+print("subtraction",num1-num2)
+print("multiply",num1*num2)
+if num2 !=0:
+    print("division",num1/num2)
+else:
+    print("division not possible")
